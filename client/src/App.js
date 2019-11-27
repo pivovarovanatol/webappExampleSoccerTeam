fca5b37beb412d1ddfa6b987d2cc874c5dd6aeff
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2019-11-27 05:26:59 +0000

    table working

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
-import Players from './components/players'
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+//import Players from './components/players'
+//import ReactTable from 'react-table'
+//import 'react-table/react-table.css'
 
 class App extends Component {
 
-  state = {
-      players:[]
+  // state = {
+  //     players:[]
+  // }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      'players': [],
+      isLoaded: false,
+    };
   }
 
-  componentDidMount() {
-    fetch('/players')
-    .then(res=>res.json())
-    .then((data) => {
-      this.setState({players: data})
-    })
-    .catch(console.log)
+
+
+  componentDidMount(){
+    fetch("/players?")
+    .then(res => res.json())
+    .then(res => this.setState({'players' : res}))
   }
-
-
   
   render() {
-
-
-
-
-
-    return (
-      //<table>
-
-        <Players players={this.state.players} />
-      //</table>
+    console.log(this.state.players);
+      return (
+      <div>
+        <table>
+          <tbody>
+          {this.state.players.map((player) => {
+            return (
+              <tr>
+              <td>{player.id}</td> 
+              <td>{player.name}</td> 
+              <td>{player.pos}</td> 
+              <td>{player.nat}</td> 
+              </tr>
+            );
+          })
+          } 
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- client/src/components/players.js
+++ client/src/components/players.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Players = ({players}) => {
     return (
-        {players : map((player) => {
-            <h1> {player} </h1>
+        <div>
+        {players.map((player) => {
+            <div>
+                <h1> {player.name} </h1>
+            </div>
         })}
+        </div>
     )
 };
 
